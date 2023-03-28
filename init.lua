8b05475ee27b29ba299dfbae897296da0abaf3c1
Danny <dannyfixit@googlemail.com>
2023-03-28 18:03:51 +0100

    Pushing laptop changes

--- init.lua
+++ init.lua
@@ -505,7 +505,26 @@ vim.g.vimwiki_global_ext = 0
     wiki.ext = '.md'
     vim.g.vimwiki_list = {wiki}
 
+-- disable netrw at the very start of your init.lua (strongly advised)
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
 
+-- set termguicolors to enable highlight groups
+vim.opt.termguicolors = true
+
+-- empty setup using defaults
+-- require("nvim-tree").setup()
+
+-- OR setup with some options
+--require("nvim-tree").setup({
+--  sort_by = "case_sensitive",
+--  renderer = {
+--    group_empty = true,
+ -- },
+ -- filters = {
+ --   dotfiles = true,
+ -- },
+--})
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
