ee93e08df74cbd1ed9d538d69269b3b54c5303e7
Danny <dannyfixit@googlemail.com>
2023-05-10 08:35:49 +0100

    Changes from desktop

--- init.lua
+++ init.lua
@@ -381,11 +381,33 @@ cmp.setup {
 }
 
 
-   local wiki = {}
-    wiki.path = '/var/www/wiki/documents'
-    wiki.syntax = 'markdown'
-    wiki.ext = '.md'
-    vim.g.vimwiki_list = {wiki}
+
+-- local wiki = {}
+-- wiki.path_html = '/var/www/wiki/documents'
+-- wiki.syntax = 'markdown'
+-- wiki.ext = '.md'
+-- vim.g.vimwiki_list = {wiki}
+-- vim.g.vim
+
+-- let vim.g.vimwiki_ext2syntax = {'.md': 'markdown', '.markdown': 'markdown', '.mdown': 'markdown'}
+-- local wiki_1 = {}
+--       wiki_1.path = '/var/www/wiki1'
+--       wiki_1.syntax = 'markdown'
+--       wiki_1.ext = '.md'
+--       wiki_1.path_html = '/var/www/wiki/documents'
+
+--  vim.g.vimwiki_list = {wiki_1}
+
+
+-- vim.g.vimwiki_list = {{path = '/var/www/wiki/documents'}}
+-- vim.g.vimwiki_ext = '.md'
+-- vim.g.vimwiki_global_ext = 0
+
+   -- local wiki = {}
+   --  wiki.path = '/var/www/wiki/documents'
+   --  wiki.syntax = 'markdown'
+   --  wiki.ext = '.md'
+   --  vim.g.vimwiki_list = {wiki}
 
 
 -- disable netrw at the very start of your init.lua (strongly advised)
--- lua/custom/plugins/vimwiki.lua
+++ lua/custom/plugins/vimwiki.lua
@@ -1,7 +1,16 @@
 
 return {
     'vimwiki/vimwiki',
-     config = function()
-        
+     config = function()  
+    --   vim.g.vimwiki_list = {{path = '/var/www/wiki/documents'}}
+    --  -- vim.g.vimwiki_list = {{path_html = '/var/www/wiki/documents'}}
+    --   vim.g.vimwiki_ext = '.md'
+    --   vim.g.vimwiki_global_ext = 0
+
+    --     local wiki = {}
+    --     wiki.path = '/var/www/wiki/documents'
+    --     wiki.syntax = 'markdown'
+    --     wiki.ext = '.md'
+    --     vim.g.vimwiki_list = {wiki}
      end
 }
