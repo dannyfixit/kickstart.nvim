e3661601c0eadeba1bbb5e2a72f9c61d32f7d576
Danny <danny.evans@better2know.com>
2023-10-07 00:34:05 +0100

    Fixed bad merge

--- lua/custom/plugins/sql.lua
+++ lua/custom/plugins/sql.lua
@@ -1,15 +1,8 @@
 return {
-<<<<<<< HEAD
     'tpope/vim-dadbod',
     'kristijanhusak/vim-dadbod-ui',
     'pbogut/vim-dadbod-ssh',
     -- config = function()
     --    let g:dbs = { "dev": "mysql://root:gameon@172.18.0.4:33066/b2k_database" }
     -- end
-=======
-  'tpope/vim-dadbod',
-  'kristijanhusak/vim-dadbod-ui',
-  config = function()
-  end,
->>>>>>> 6eecb41f5f18145bcc0fdbb9a167a5f3674f47aa
 }
