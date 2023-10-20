2b0596d81e5f856b93ee51cfe20e5464c8aafc67
Danny <dannyfixit@googlemail.com>
2023-10-20 09:04:50 +0100

    Changes form Desktop

--- lua/custom/plugins/indent-blankline.lua
+++ lua/custom/plugins/indent-blankline.lua
@@ -1,10 +1,24 @@
- -- Add indentation guides even on blank lines
-return  {
-  'lukas-reineke/indent-blankline.nvim',
+-- Add indentation guides even on blank lines
+local highlight = {
+    "CursorColumn",
+    "Whitespace",
+}
+
+return {
+    'lukas-reineke/indent-blankline.nvim',
     -- Enable `lukas-reineke/indent-blankline.nvim`
     -- See `:help indent_blankline.txt`
-    opts = {
-      char = '┊',
-      show_trailing_blankline_indent = false,
+    main = "ibl",
+    setup = {
+        indent = { highlight = highlight, char = "┊" },
+        whitespace = {
+            highlight = highlight,
+            remove_blankline_trail = false,
+        },
+        scope = { enabled = false },
     }
-  }
+    -- opts = {
+    -- char = '┊',
+    -- show_trailing_blankline_indent = false,
+    -- }
+}
